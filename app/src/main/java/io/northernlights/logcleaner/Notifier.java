42b75035ae9c0e1732c40a37e3ad6d4929f488da
Alessandro Garzaro <alessandro@codeshards.net>
2015-01-12 19:23:09 +0100

    Added feedback to notifications

--- app/src/main/java/io/northernlights/logcleaner/Notifier.java
+++ app/src/main/java/io/northernlights/logcleaner/Notifier.java
@@ -19,7 +19,10 @@ package io.northernlights.logcleaner;
  */
 
 import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.TaskStackBuilder;
 import android.content.Context;
+import android.content.Intent;
 import android.support.v4.app.NotificationCompat;
 
 public class Notifier {
@@ -31,6 +34,14 @@ public class Notifier {
                         .setContentTitle(title)
                         .setContentText(text);
 
+
+        Intent resultIntent = new Intent(context, MainActivity.class);
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(context);
+        stackBuilder.addParentStack(MainActivity.class);
+        stackBuilder.addNextIntent(resultIntent);
+        PendingIntent resultPendingIntent =
+                stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
+        mBuilder.setContentIntent(resultPendingIntent);
 
         NotificationManager mNotificationManager =
                 (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
